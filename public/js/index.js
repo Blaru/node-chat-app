f98ab9cf1a52a5e6e899b0f1cd57bf86153e9bbd
Alexandre R. Pelagaggi Nunes <pelagaggialex@gmail.com>
2018-02-17 18:35:21 -0200

    Added custom events for chat app

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+var socket = io();
+
+socket.on('connect',function(){
+  console.log('Connected to server');
+});
+
+socket.on('disconnect',function(){
+  console.log('Client disconnected to server');
+});
+
+socket.on('newMessage',function(message){
+  console.log('new Message');
+  console.log(JSON.stringify(message,undefined,2));
+  message.from = 'chrome@example.com';
+  socket.emit('createMessage',message)
+});
--- server/server.js
+++ server/server.js
@@ -8,13 +8,27 @@ const port = process.env.PORT || 3000;
 const app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var client_n =0;
 
 app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
+  var id = client_n++;
   console.log('New user connected');
-
+  setInterval(()=>{
+    socket.emit('newMessage',{
+      from:'server@example.com',
+      content:'Example',
+      id,
+      createdAt : new Date()
+    });
+  },10000);
+  socket.on('createMessage',(message)=>{
+    message.createdAt = new Date();
+    console.log(JSON.stringify(message,undefined,2));
+  });
   socket.on('disconnect',()=>{
+    client_n--;
     console.log('Client disconnected to server');
   });
 });
