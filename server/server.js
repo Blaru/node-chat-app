411bdf78ec7cb757334e116e77a20761abdbccdd
Alexandre R. Pelagaggi Nunes <pelagaggialex@gmail.com>
2018-02-17 19:22:45 -0200

    Add welcome message and broadcast new user

--- server/server.js
+++ server/server.js
@@ -11,14 +11,22 @@ var io = socketIO(server);
 var client_n =0;
 
 app.use(express.static(publicPath));
-
+class message {
+  constructor(from, text){
+  this.from = from;
+  this.text = text;
+  this.createdAt= new Date().getTime();
+}};
 io.on('connection',(socket)=>{
   var id = client_n++;
+  socket.emit('newMessage',new message('Admin','Welcome to chat app'));
+  socket.broadcast.emit('newMessage',new message('Admin',`Client:${id} has joined the chat}`));
   console.log('New user connected');
   socket.on('createMessage',(message)=>{
-    message.createdAt = new Date();
+    message.createdAt = new Date().getTime();
     console.log(JSON.stringify(message,undefined,2));
-    io.emit('newMessage',message);
+    //io.emit('newMessage',message);
+    socket.broadcast.emit('newMessage',message);
   });
   socket.on('disconnect',()=>{
     client_n--;
